fdd3c629f65785d7cc11ff58587ba3a71caf53c1
GhostFir31 <j3sus0marle@gmail.com>
2023-04-14 15:48:25 -0700

    creacion clase un

---
+++ src/Carta.java
@@ -0,0 +1,44 @@
+
+
+public class Carta {
+
+    private String figura;
+    private String color;
+    private int numero;
+
+    public Carta(String figura, String color, int numero) {
+        this.figura = figura;
+        this.color = color;
+        this.numero = numero;
+    }
+
+    public Carta generarCartaAleatoria() {
+       Random valorAleatorio
+
+        return Carta;
+    }
+
+    public String getFigura() {
+        return this.figura;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setFigura(String figura) {
+        this.figura = figura;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+}
